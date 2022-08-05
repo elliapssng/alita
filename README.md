09da314565dab9b3b481a669559307a07e5d5e80
elliapssng <56740211+elliapssng@users.noreply.github.com>
2022-08-05 23:16:12 +0200

    build: stack docker (#1)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# alita
+# Alita
+
+## Install project 
+
+1. You must be to install CA and certificat wildcard : [show documentation](./docs/mkcert/readme.md)
+2. Follow [docker - install](./docs/docker/install.md) for have docker stack
---
+++ docker/php/docker-entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- php-fpm "$@"
+fi
+
+if [ "$INSTALL" = true ]; then
+    echo "*********** INSTALL = true *******************"
+    echo "*********** COMPOSER *******************"
+    composer install --prefer-dist --no-interaction --no-scripts
+    echo "*********** YARN && ENCORE *******************"
+    yarn install
+    yarn encore dev
+    echo "*********** CREATION DES DOSSIERS *******************"
+    mkdir -p /srv/alita/var
+    mkdir -p /srv/alita/public/upload/pictures
+    echo "*********** MAPPAGE DE L'UTILISATEUR VERS WWW-DATA *******************"
+    ## Remapping generate folders to match user id when volume is mounted
+    if [ "$(id -u www-data)" = "82" ]; then
+        deluser --remove-home www-data &&\
+        adduser -u "${USER_ID}" -D www-data -s /bin/sh
+        chown -h -R "${USER_ID}":"${USER_ID}" \
+        /srv/alita/var \
+        /srv/alita/public \
+        /srv/alita/data \
+        /srv/alita/vendor \
+        /srv/alita/node_modules
+    fi
+else
+    echo "*********** INSTALL = false *******************"
+fi
+
+exec "$@"
